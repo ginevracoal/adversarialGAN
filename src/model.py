6f1ff482c43d7ca04bcb570f5eba8c558f74207d
Francesco Franchina <cescus92@gmail.com>
2020-02-18 13:39:07 +0100

    Added physical limits to acceleration and speed

--- src/model.py
+++ src/model.py
@@ -4,8 +4,17 @@ import numpy as np
 
 from diffquantitative import DiffQuantitativeSemantic
 
+def ranged(x, max_x, min_x=None):
+    if min_x:
+        return torch.min(torch.max(x, torch.tensor(min_x)), torch.tensor(max_x))
+    else:
+        return torch.min(x, torch.tensor(max_x))
+
 class Car:
     def __init__(self):
+        self._max_acceleration = 10.0
+        self._min_acceleration = -self._max_acceleration
+        self._max_velocity = 150.0
         self.mass = 1.0
         self.position = 0.0
         self.velocity = 0.0
@@ -13,17 +22,16 @@ class Car:
         self.friction_coefficient = 0.1
 
     def update(self, in_acceleration, dt):
-        self.acceleration = in_acceleration
+        self.acceleration = ranged(in_acceleration, self._max_acceleration, self._min_acceleration)
         if self.velocity > 0:
             self.acceleration -= self.friction_coefficient * self.mass
-        self.velocity += self.acceleration * dt
+        self.velocity += ranged(self.acceleration * dt, self._max_velocity)
         self.position += self.velocity * dt
 
 
 class Environment:
     def __init__(self):
         self._leader_car = Car()
-        self._max_acceleration = 10.0
 
     def set_agent(self, agent):
         self._agent = agent
@@ -49,14 +57,13 @@ class Environment:
 
     def update(self, parameters, dt):
         # the environment updates according to the parameters
-        pedal = parameters[0]
-        self._leader_car.update(pedal * self._max_acceleration, dt)
+        acceleration = parameters[0]
+        self._leader_car.update(acceleration, dt)
 
 
 class Agent:
     def __init__(self):
         self._car = Car()
-        self._max_acceleration = 10.0
 
     def set_environment(self, environment):
         self._environment = environment
@@ -86,8 +93,8 @@ class Agent:
 
     def update(self, parameters, dt):
         # the action take place and updates the variables
-        pedal = parameters[0]
-        self._car.update(pedal * self._max_acceleration, dt)
+        acceleration = parameters[0]
+        self._car.update(acceleration, dt)
 
 
 class Model:
