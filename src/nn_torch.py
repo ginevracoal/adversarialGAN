3e5d14a981969283a83615bc11c56c04f4eb5581
Francesco Franchina <cescus92@gmail.com>
2020-02-18 13:47:37 +0100

    Improved debugging (tensorboard) in the NN architecture

--- src/nn_torch.py
+++ src/nn_torch.py
@@ -49,8 +49,6 @@ class Trainer:
             self.log = SummaryWriter(logging_dir)
 
     def train_attacker_step(self, time_horizon, dt):
-        self.model.initialize_random()
-
         z = torch.rand(5)
         o = torch.tensor(self.model.get_status())
 
@@ -80,11 +78,9 @@ class Trainer:
 
         self.defender_optimizer.step()
 
-        return rho
+        return float(loss.detach())
 
     def train_defender_step(self, time_horizon, dt):
-        self.model.initialize_random()
-
         z = torch.rand(5)
         o = torch.tensor(self.model.get_status())
 
@@ -114,15 +110,16 @@ class Trainer:
 
         self.defender_optimizer.step()
 
-        return rho
+        return float(loss.detach())
 
     def train(self, iteration, time_horizon, dt):
-        atk_rho = self.train_attacker_step(time_horizon, dt)
-        def_rho = self.train_defender_step(time_horizon, dt)
+        self.model.initialize_random()
+        atk_loss = self.train_attacker_step(time_horizon, dt)
 
-        if self.logging and (iteration % 2) == 0:
-            self.log.add_scalar('attacker ρ', atk_rho, iteration)
-            self.log.add_scalar('defender ρ', def_rho, iteration)
+        self.model.initialize_random()
+        def_loss = self.train_defender_step(time_horizon, dt)
+
+        return (atk_loss, def_loss)
 
     def test_step(self, dt):
         with torch.no_grad():
@@ -140,9 +137,30 @@ class Trainer:
 
     def run(self, n_steps, time_horizon=100, dt=0.05):
 
-        for i in tqdm(range(n_steps)):
-            self.train(i, time_horizon, dt)
+        if self.logging:
+            hist_every = int(n_steps / 3)
+            hist_counter = 0
 
+            atk_loss_vals = np.zeros(n_steps)
+            def_loss_vals = np.zeros(n_steps)
+
+        for i in tqdm(range(n_steps)):
+            atk_loss, def_loss = self.train(i, time_horizon, dt)
+
+            if self.logging:
+                atk_loss_vals[i] = atk_loss
+                def_loss_vals[i] = def_loss
+
+                self.log.add_scalar('attacker loss', atk_loss, i)
+                self.log.add_scalar('defender loss', def_loss, i)
+
+                if (i + 1) % hist_every == 0:
+                    a = hist_counter * hist_every
+                    b = (hist_counter + 1) * hist_every
+                    hist_counter += 1
+
+                    self.log.add_histogram('attacker loss hist', atk_loss_vals[a:b], i)
+                    self.log.add_histogram('defender loss hist', def_loss_vals[a:b], i)
             #self.test(n_steps, dt)
             #test_rho = self.robustness_computer.compute(self.model)
 
