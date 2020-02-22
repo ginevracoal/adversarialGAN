48b1c01f0de018a53e25016cf9641a8985c1b933
Francesco Franchina <cescus92@gmail.com>
2020-02-22 23:22:39 +0100

    Introduced ParametersHyperparallelepiped to specify parameter's distributions

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import os
 import torch
+import itertools
+import numpy as np
 
 def save_models(attacker_model, defender_model, path):
     destination = os.path.join(path, 'models')
@@ -20,3 +22,29 @@ def load_models(attacker_model, defender_model, path):
     attacker_model.load_state_dict(torch.load(atk_path))
     defender_model.load_state_dict(torch.load(def_path))
 
+
+class ParametersHyperparallelepiped:
+
+    def __init__(self, *ranges):
+        self._ranges = ranges
+
+    def _all(self, times):
+        for i in range(times):
+            l = list(itertools.product(*self._ranges))
+            np.random.shuffle(l)
+            for p in l:
+                yield p
+
+    def _random(self, n):
+        for i in range(n):
+            yield [np.random.choice(r) for r in self._ranges]
+
+    def sample_all(self, times=1, mu=0, sigma=1):
+        for p in self._all(times):
+            n = np.random.normal(mu, sigma)
+            yield [x + n for x in p]
+
+    def sample_random(self, n=100, mu=0, sigma=1):
+        for p in self._random(n):
+            n = np.random.normal(mu, sigma)
+            yield [x + n for x in p]
