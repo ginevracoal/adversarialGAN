344d4042bdebf9d4269b9c51008af02f3e6ff6f7
Francesco Franchina <cescus92@gmail.com>
2020-03-16 20:42:30 +0100

    Added scripts to produce the plots

---
+++ src/plotter_platooning.py
@@ -0,0 +1,89 @@
+import os
+import pickle
+
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-d", "--dir", dest="dirname",
+                    help="model's directory")
+parser.add_argument("--triplots", default=False, action="store_true" , help="Generate triplots")
+parser.add_argument("--hist", default=False, action="store_true" , help="Generate histograms")
+args = parser.parse_args()
+    
+with open(os.path.join(args.dirname, 'sims.pkl'), 'rb') as f:
+    records = pickle.load(f)
+
+def hist(pulse, step_up, step_down, atk, filename):
+    fig, ax = plt.subplots(1, 4, figsize=(12, 3), sharex=True)
+
+    config = {
+        'norm_hist': True,
+        #'bins': range(0, 100, 5),
+        'color': 'b',
+        'kde': False,
+        'axlabel': '% of $dist$ in limits',
+    }
+    sns.distplot(step_up * 100, **config, ax=ax[0]).set_title('Sudden acceleration')
+    sns.distplot(step_down * 100, **config, ax=ax[1]).set_title('Sudden brake')
+    sns.distplot(pulse * 100, **config, ax=ax[2]).set_title('Acceleration pulse')
+    sns.distplot(atk * 100, **config, ax=ax[3]).set_title('Against attacker')
+
+    fig.tight_layout()
+    fig.savefig(os.path.join(args.dirname, filename), dpi=150)
+
+def plot(sim_time, sim_agent_pos, sim_agent_dist, sim_agent_acc, sim_env_pos, sim_env_acc, filename):
+    fig, ax = plt.subplots(1, 3, figsize=(12, 3))
+
+    ax[0].plot(sim_time, sim_agent_pos, label='follower')
+    ax[0].plot(sim_time, sim_env_pos, label='leader')
+    ax[0].set(xlabel='time (s)', ylabel='position (m)')
+    ax[0].legend()
+
+    ax[1].plot(sim_time, sim_agent_dist)
+    ax[1].set(xlabel='time (s)', ylabel='distance (m)')
+    ax[1].axhline(2, ls='--', color='r')
+    ax[1].axhline(10, ls='--', color='r')
+
+    ax[2].plot(sim_time, np.clip(sim_agent_acc, -3, 3), label='follower')
+    ax[2].plot(sim_time, np.clip(sim_env_acc, -3, 3), label='leader')
+    ax[2].set(xlabel='time (s)', ylabel='acceleration ($m/s^2$)')
+    ax[2].legend()
+
+    fig.tight_layout()
+    fig.savefig(os.path.join(args.dirname, filename), dpi=150)
+
+if args.triplots:
+    print('pulse:', records[0]['pulse']['init'])
+    plot(records[0]['pulse']['sim_t'], records[0]['pulse']['sim_ag_pos'], records[0]['pulse']['sim_ag_dist'], records[0]['pulse']['sim_ag_acc'], records[0]['pulse']['sim_env_pos'], records[0]['pulse']['sim_env_acc'], 'triplot_pulse.png')
+
+    print('step_up:', records[0]['step_up']['init'])
+    plot(records[0]['step_up']['sim_t'], records[0]['step_up']['sim_ag_pos'], records[0]['step_up']['sim_ag_dist'], records[0]['step_up']['sim_ag_acc'], records[0]['step_up']['sim_env_pos'], records[0]['step_up']['sim_env_acc'], 'triplot_step_up.png')
+
+    print('step_down:', records[0]['step_down']['init'])
+    plot(records[0]['step_down']['sim_t'], records[0]['step_down']['sim_ag_pos'], records[0]['step_down']['sim_ag_dist'], records[0]['step_down']['sim_ag_acc'], records[0]['step_down']['sim_env_pos'], records[0]['step_down']['sim_env_acc'], 'triplot_step_down.png')
+
+    print('attacker:', records[0]['atk']['init'])
+    plot(records[0]['atk']['sim_t'], records[0]['atk']['sim_ag_pos'], records[0]['atk']['sim_ag_dist'], records[0]['atk']['sim_ag_acc'], records[0]['atk']['sim_env_pos'], records[0]['atk']['sim_env_acc'], 'triplot_attacker.png')
+
+if args.hist:
+    size = len(records)
+    pulse_pct = np.zeros(size)
+    step_up_pct = np.zeros(size)
+    step_down_pct = np.zeros(size)
+    atk_pct = np.zeros(size)
+
+    for i in range(size):
+        t = records[i]['pulse']['sim_ag_dist']
+        pulse_pct[i] = np.sum(np.logical_and(t > 2, t < 10)) / len(t)
+        t = records[i]['step_up']['sim_ag_dist']
+        step_up_pct[i] = np.sum(np.logical_and(t > 2, t < 10)) / len(t)
+        t = records[i]['step_down']['sim_ag_dist']
+        step_down_pct[i] = np.sum(np.logical_and(t > 2, t < 10)) / len(t)
+        t = records[i]['atk']['sim_ag_dist']
+        atk_pct[i] = np.sum(np.logical_and(t > 2, t < 10)) / len(t)
+
+    hist(pulse_pct, step_up_pct, step_down_pct, atk_pct, 'pct_histogram.png')
---
+++ src/tester_platooning.py
@@ -0,0 +1,106 @@
+import os
+import pickle
+
+import model_platooning
+import misc
+import architecture
+
+import torch
+import torch.nn as nn
+import numpy as np
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument("-d", "--dir", dest="dirname",
+                    help="model's directory")
+parser.add_argument("-r", "--repetitions", dest="repetitions", type=int, default=1,
+                    help="simulation repetions")
+args = parser.parse_args()
+
+agent_position = 0
+agent_velocity = np.linspace(0, 20, 10)
+leader_position = np.linspace(1, 12, 15)
+leader_velocity = np.linspace(0, 20, 10)
+pg = misc.ParametersHyperparallelepiped(agent_position, agent_velocity, leader_position, leader_velocity)
+
+physical_model = model_platooning.Model(pg.sample(sigma=0.05))
+
+robustness_formula = 'G(dist <= 10 & dist >= 2)'
+robustness_computer = model_platooning.RobustnessComputer(robustness_formula)
+
+attacker = architecture.Attacker(physical_model, 2, 10, 2)
+defender = architecture.Defender(physical_model, 2, 10)
+
+misc.load_models(attacker, defender, args.dirname)
+
+dt = 0.05
+steps = 300
+
+def run(mode=None):
+    physical_model.initialize_random()
+    conf_init = {
+        'ag_pos': physical_model.agent.position,
+        'ag_vel': physical_model.agent.velocity,
+        'env_pos': physical_model.environment.l_position,                     
+        'env_vel': physical_model.environment.l_velocity,
+    }
+
+    sim_t = []
+    sim_ag_pos = []
+    sim_ag_dist = []
+    sim_ag_acc = []
+    sim_env_pos = []
+    sim_env_acc = []
+
+    t = 0
+    for i in range(steps):
+        with torch.no_grad():
+            oa = torch.tensor(physical_model.agent.status)
+            oe = torch.tensor(physical_model.environment.status)
+            z = torch.rand(attacker.noise_size)
+            if mode == 0:
+                atk_policy = lambda x: torch.tensor(2.) if i > 200 and i < 250 else torch.tensor(-2.)
+            elif mode == 1:
+                atk_policy = lambda x: torch.tensor(2.) if i > 150 else torch.tensor(-2.)
+            elif mode == 2:
+                atk_policy = lambda x: torch.tensor(2.) if i < 150 else torch.tensor(-2.)
+            else:
+                atk_policy = attacker(torch.cat((z, oe)))
+            def_policy = defender(oa)
+
+        atk_input = atk_policy(dt)
+        def_input = def_policy(dt)
+
+        physical_model.step([atk_input], [def_input], dt)
+
+        sim_ag_acc.append(def_input)
+        sim_env_acc.append(atk_input)
+        sim_t.append(t)
+        sim_ag_pos.append(physical_model.agent.position)
+        sim_env_pos.append(physical_model.environment.l_position)
+        sim_ag_dist.append(physical_model.agent.distance)
+
+        t += dt
+        
+    return {'init': conf_init,
+            'sim_t': np.array(sim_t),
+            'sim_ag_pos': np.array(sim_ag_pos),
+            'sim_ag_dist': np.array(sim_ag_dist),
+            'sim_ag_acc': np.array(sim_ag_acc),
+            'sim_env_pos': np.array(sim_env_pos),
+            'sim_env_acc': np.array(sim_env_acc),
+    }
+
+records = []
+for i in range(args.repetitions):
+    sim = {}
+    sim['pulse'] = run(0)
+    sim['step_up'] = run(1)
+    sim['step_down'] = run(2)
+    sim['atk'] = run()
+    
+    records.append(sim)
+    
+with open(os.path.join(args.dirname, 'sims.pkl'), 'wb') as f:
+    pickle.dump(records, f)
